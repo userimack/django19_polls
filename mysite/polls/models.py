879a1daa43779da0cfdc245bbafe57947094db87
userimack <mahendra.k12@gmail.com>
2016-01-03 18:23:35 +0530

    Modified admin template and added static files

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -10,9 +10,15 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days = 1) <= self.pub_date <=now
     def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days = 1) <= self.pub_date <=now
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
